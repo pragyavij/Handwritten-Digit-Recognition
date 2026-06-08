b75779d1263ead2a0ccb067af39758fbac944786
pragyavij <pragyav0695@gmail.com>
2026-06-08 13:40:55 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,10 @@
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -6,19 +12,19 @@ X_train = X_train / 255.0
 X_test = X_test / 255.0
 
 
-X_train = X_train.reshape(-1,28,28,1)
-X_test = X_test.reshape(-1,28,28,1)
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1
+)
+
+datagen.fit(X_train)
 
 print(X_train.shape)
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
-
 model = Sequential()
-
 model.add(
     Conv2D(
         filters=32,
@@ -40,27 +46,38 @@ model.add(
 
 model.add(MaxPooling2D(pool_size=(2,2)))
 
+model.add(
+    Conv2D(
+        filters=128,
+        kernel_size=(3,3),
+        activation='relu'
+    )
+)
 model.add(Flatten())
 
-model.add(Dense(128,activation='relu'))
+model.add(Dense(128, activation='relu'))
 
-model.add(Dense(10,activation='softmax'))
-
+model.add(Dense(10, activation='softmax'))
 model.summary()
-
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
 )
 history = model.fit(
-    X_train,
-    y_train,
-    epochs=5,
-    validation_data=(X_test,y_test)
+    datagen.flow(
+        X_train,
+        y_train,
+        batch_size=32
+    ),
+    epochs=10,
+    validation_data=(X_test, y_test)
 )
-loss, accuracy = model.evaluate(X_test,y_test)
+loss, accuracy = model.evaluate(
+    X_test,
+    y_test
+)
 
-print("Accuracy:", accuracy)
-
-model.save("models/mnist_cnn.h5")
+print("Test Accuracy:", accuracy)
+model.save("models/mnist_cnn.keras")
+print("Model Saved Successfully")
